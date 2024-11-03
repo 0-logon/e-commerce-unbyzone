86e60da3eb9df42b99e0673a8dd3e8bdc1efbff8
markotomic <masheritto@gmail.com>
2024-11-03 03:48:51 +0100

    Cart update 🌵

--- src/components/shared/navbar/Cart.jsx
+++ src/components/shared/navbar/Cart.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useEffect, useRef, useState } from 'react';
 import styles from '@/styles/shared/Cart.module.css';
 import { CartIcon, CloseIcon, DeleteBinIcon } from '@/assets/icons/icons';
 import { useDispatch, useSelector } from 'react-redux';
@@ -6,13 +6,16 @@ import DataContext from '@/store/context/DataContext';
 import { useQuery } from '@apollo/client';
 import { GET_CART_PRODUCT } from '@/graphql/queries';
 import { removeItem } from '@/store/slices/cartSlice';
+import Link from 'next/link';
 
 const Cart = () => {
     const [modal, setModal] = useState(false);
+    const modalRef = useRef(null);
     const dispatch = useDispatch();
     const totalQuantity = useSelector(state => state.cart.totalQuantity);
     const cartItems = useSelector(state => state.cart.items);
     const { setContextData, products } = useContext(DataContext);
+    const [totalPrice, setTotalPrice] = useState(0);
 
     const handleModal = () => {
         setModal(!modal);
@@ -43,18 +46,45 @@ const Cart = () => {
         dispatch(removeItem(id));
     };
 
+    useEffect(() => {
+        let totalPrice = 0;
+        cartProductsData.forEach(product => {
+            totalPrice += product.price * product.quantity;
+        });
+        setTotalPrice(totalPrice);
+    }, [cartProductsData]);
+
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (modalRef.current && !modalRef.current.contains(event.target)) {
+                setModal(false);
+            }
+        };
+
+        if (modal) {
+            document.addEventListener("mousedown", handleClickOutside);
+        } else {
+            document.removeEventListener("mousedown", handleClickOutside);
+        }
+
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, [modal]);
+
     return (
         <div className={styles.component}>
             <div className={styles.handler}>
                 <button onClick={() => handleModal()} className={styles.toggle}>
-                    <div className={styles.handler_indicator}>
+                    {totalQuantity > 0 && <div className={styles.handler_indicator}>
                         <p>{totalQuantity}</p>
-                    </div>
+                    </div>}
+
                     <CartIcon />
                 </button>
             </div>
             {modal && <div className={styles.modal_area}>
-                <div className={styles.modal}>
+                <div ref={modalRef} className={styles.modal}>
                     <div className={styles.modal_header}>
                         <p>Your Cart</p>
                         <button onClick={handleModal}>
@@ -63,9 +93,9 @@ const Cart = () => {
                     </div>
                     <div className={styles.modal_body}>
                         {
-                            cartProductsData.map((product) => (
+                            cartProductsData.length ? cartProductsData.map((product) => (
                                 <div key={product.id} className={styles.item}>
-                                    <div className={styles.left_box}>
+                                    <Link href={`/product/${product.slug}`} onClick={handleModal} className={styles.left_box}>
                                         <div className={styles.img_box}>
                                             <img src={product.photo.url} alt={product.title} />
                                         </div>
@@ -74,11 +104,21 @@ const Cart = () => {
                                             <span>{new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(product.price)}</span>
                                             <span>Quantity: {product.quantity}</span>
                                         </div>
+                                    </Link>
+                                    <div className={styles.delete_button}>
+                                        <button onClick={() => handleRemoveItem(product.id)}><DeleteBinIcon /></button>
                                     </div>
-                                    <button onClick={()=> handleRemoveItem(product.id)}><DeleteBinIcon /></button>
                                 </div>
-                            ))
+                            )) : <p>Your cart is empty. Start shopping!</p>
                         }
+                    </div>
+                    <div className={styles.modal_footer}>
+                        <div className={styles.total_data}>
+                            <p>
+                                Total ({totalQuantity} {cartProductsData.length === 1 ? "item" : "items"}): {new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(totalPrice)}
+                            </p>
+                        </div>
+                        {totalQuantity > 0 ? <Link href="/">Checkout</Link> : <Link href="/">Start shopping</Link>}
                     </div>
                 </div>
             </div>}
